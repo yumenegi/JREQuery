a99e3b97cba8d4f8fa0837ed94c350e711d6b261
yumenegi <shuumatsupvocaloid@gmail.com>
2025-03-05 17:50:57 -0600

    Added ability to query current timetable of specific train lines given Ekitan link

--- ekitan.py
+++ ekitan.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style
+import datetime
+from jrequery import JREQuery, JRERegion
 
-eki = requests.get("https://ekitan.com/timetable/railway/line-station/182-11/d2?view=list", headers={'User-Agent': 'Mozilla/5.0'})
+url = "https://ekitan.com/timetable/railway/line-station/151-0/d1?view=list"
+eki = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(eki.content, "html.parser")
 
 active = soup.find("div", class_="active")
@@ -15,5 +18,21 @@ euy = [i.split('\n') for i in euy]
 for i, y in enumerate(euy):
     euy[i] = list(filter(None, map(str.strip, y)))
 
-for i in euy:
-    print(i)
+# for i in euy:
+#     print(i)
+
+t = datetime.datetime.now(datetime.timezone.utc)
+t += datetime.timedelta(hours=9)
+
+for i, it in enumerate(euy):
+    train_t = it[0].split(':')
+    # print(train_t)
+    if (int(train_t[0]) >= t.hour and int(train_t[1]) >= t.minute):
+        break
+
+for i in euy[i:i+8]:
+    print(" | ".join(i))
+
+jreq = JREQuery.JREQuery(JRERegion.SHINETSU, debug=False)
+if jreq["只見線"] != "平常運転":
+    print(Fore.RED+jreq["只見線"]+Fore.WHITE)
--- jrequery.py
+++ jrequery.py
@@ -1,22 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style
+from enum import Enum
 
-x = requests.get("https://traininfo.jreast.co.jp/train_info/tohoku.aspx", headers={'User-Agent': 'Mozilla/5.0'})
+class JRERegion:
+    KANTO = "kanto"
+    SHINETSU = "shinetsu"
+    TOHOKU = "tohoku"
+    CHYOKYORI = "chyokyori"
+    SHINKANSEN = "shinkansen"
 
-soup = BeautifulSoup(x.content, 'html.parser')
 
-s = soup.find_all('span', class_='name')
-JRELines = [line.get_text().strip() for line in s]
+class JREQuery:
+    def JREQuery(region, debug = False) -> dict:
+        x = requests.get("https://traininfo.jreast.co.jp/train_info/%s.aspx" % region, headers={'User-Agent': 'Mozilla/5.0'})
 
-s = soup.find_all('div', class_='rosen_infoBox')
-JRENotices = [info.get_text().strip() for info in s]
+        soup = BeautifulSoup(x.content, 'html.parser')
 
-JREQuery = {}
-for i, j in zip(JRELines, JRENotices):
-    if (i not in JREQuery.keys()):
-        JREQuery[i] = j.replace("\n\n","：")
+        s = soup.find_all('span', class_='name')
+        JRELines = [line.get_text().strip() for line in s]
 
-for key, val in JREQuery.items():
-    print("%s: %s" % (Fore.RED + key if val != "平常運転" else Fore.GREEN+key, val))
-# print(JREQuery)
+        s = soup.find_all('div', class_='rosen_infoBox')
+        JRENotices = [info.get_text().strip() for info in s]
+
+        res = {}
+        for i, j in zip(JRELines, JRENotices):
+            if (i not in res.keys()):
+                res[i] = j.replace("\n\n","：")
+
+        if (debug):
+            for key, val in res.items():
+                print("%s: %s" % (Fore.RED + key if val != "平常運転" else Fore.GREEN+key, val))
+        return res
